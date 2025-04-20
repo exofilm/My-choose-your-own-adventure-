9f3a2c79c175087fb14c81912628db4d14ce3deb
exofilm <tyler.mcnair976@gmail.com>
2025-04-20 15:22:07 -0600

    Changed all inputs to be lowercase

--- adventure.py
+++ adventure.py
@@ -24,13 +24,13 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-choice1 = input('You are nearing the edge of the forest, would you like to go left or right?')
+choice1 = input('You are nearing the edge of the forest, would you like to go left or right?').lower()
 if choice1 == "left":
     print('You are safe.... for now.')
-    choice2 = input("You find yourself overlooking a rapid river. Would you like to 'swim' through it, or 'wait' for a boat?")
+    choice2 = input("You find yourself overlooking a rapid river. Would you like to 'swim' through it, or 'wait' for a boat?").lower()
     if choice2 == 'wait':
         print('A boat arrives and takes you safely across the river')
-        choice3 = input('You made it across the river. Congrats nerd! On the other side of the river, you find three doors. One is red, one is blue, and one is yellow. Which door would you like to go through?' )
+        choice3 = input('You made it across the river. Congrats nerd! On the other side of the river, you find three doors. One is red, one is blue, and one is yellow. Which door would you like to go through?' ).lower()
         if choice3 == "red":
             print('It was a bomb. You blew up. That is the whole door. Do you really think this treasure is worth it?')
         elif choice3 == "blue":
